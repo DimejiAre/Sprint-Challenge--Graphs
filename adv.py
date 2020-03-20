9f566f1b6261a6fcc9e98c5944f3588fb6c4bc07
DimejiAre <olardi2@gmail.com>
2020-03-20 11:05:50 +0100

    add bfs for finding unexplored room

--- adv.py
+++ adv.py
@@ -4,6 +4,7 @@ from world import World
 
 import random
 from ast import literal_eval
+from util import Queue, Stack
 
 # Load world
 world = World()
@@ -12,9 +13,9 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,12 +30,58 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+graph = {}
+
+# create dictionary for each room when visited
+def create_room():
+    room = {}
+    for exit in player.current_room.get_exits():
+        room[exit] = "?"
+        graph[player.current_room.id] = room
+
+# Algorithm to find random exit that hasn't been explored yet
+def current_room_unexplored_exit():
+    unexplored = []
+    for exit in player.current_room.get_exits():
+        if graph[player.current_room.id][exit] == "?":
+            unexplored.append(exit)
+
+    return random.choice(unexplored)
+
+def path_to_unexplored_room(starting_room):
+    q = Queue()
+    q.enqueue([starting_room])
+    visited = set()
+
+    while q.size() > 0:
+        path = q.dequeue()
+        room = path[-1]
+
+        if list(graph[room].values()).count('?') != 0:
+            return path
+        if room not in visited:
+            visited.add(room)
+
+            for next_room in graph[room].values():
+                new_path = path.copy()
+                new_path.append(next_room)
+                q.enqueue(new_path)
+
+
+
+print("graph: ", graph)
+print("traversal path", traversal_path)
 
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
+
+# print("currentroom id", player.current_room.id)
+# print("current room: ", player.current_room.get_exits())
+# print("visited rooms: ", visited_rooms)
+# print(len(room_graph), len(graph))
 
 for move in traversal_path:
     player.travel(move)
@@ -51,12 +98,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
